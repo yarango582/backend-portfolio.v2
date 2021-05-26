15f26a363da8168f86347f54549734e028e37d0d
yarangodev <yeisonarango03@gmail.com>
2021-05-25 19:38:27 -0500

    probando

--- src/routes/router.js
+++ src/routes/router.js
@@ -2,16 +2,6 @@ const express = require('express');
 
 const router = express.Router();
 
-router.use(('/', (req, res) => {
-    res.send(`
-        <div style='position:relative; width:100%; height: 100vh;'>
-            <img
-            src='https://image.freepik.com/vector-gratis/poster-error-404-pagina-no-encontrada-usar-sitio-web_1284-49337.jpg'
-            />
-        <div/>
-    `);
-}));
-
 router.get('/prueba', (req, res) => {
     res.status(200).json({message: 'todo ok'});
 })
